f31d65d68e92356acd2518852d5e3105c70e507a
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-04-30 23:53:34 +0300

    To - do listesinden eleman silme işlemleri tamamlandı

--- js/main.js
+++ js/main.js
@@ -1,9 +1,21 @@
 function addTask(){
     const input = document.getElementById("textInput");
     const value = input.value;
+
     if (value === "") return;
+
     const li = document.createElement("li");
     li.textContent = value;
+
+    const btn = document.createElement("button");
+    btn.textContent = "SİL";
+
+    btn.onclick = function(){
+        li.remove();
+    }
+
+    li.appendChild(btn);
+
     document.getElementById("taskList").appendChild(li);
     input.value = "";
 }
