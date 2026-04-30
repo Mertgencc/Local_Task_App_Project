b33f3db5d252a7b6b1b9ea69ce75ed342e82d00e
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-04-30 23:03:33 +0300

    To - do listesine eleman ekleme bölümü yapıldı

---
+++ js/main.js
@@ -0,0 +1,9 @@
+function addTask(){
+    const input = document.getElementById("textInput");
+    const value = input.value;
+    if (value === "") return;
+    const li = document.createElement("li");
+    li.textContent = value;
+    document.getElementById("taskList").appendChild(li);
+    input.value = "";
+}
