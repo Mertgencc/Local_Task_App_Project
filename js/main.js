d9223163e5fff74ae15bf56f36ee7dfcf83c4af9
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-05-01 21:04:05 +0300

    Bootstrap ve CSS Ayarlamaları Yapıldı

--- js/main.js
+++ js/main.js
@@ -1,21 +1,22 @@
 function addTask(){
-    const input = document.getElementById("textInput");
-    const value = input.value;
+    const input = document.getElementById("textInput");  // burada ise ben textInput alanımı çağırdım.
+    const value = input.value; // burada da input alanımın içine oluşturduğum value değerimi girmeye başladım.
+    
+    if (value === "") return; // burada ise kullanıcı değer girmezse ekran bir şey dönmez.
 
-    if (value === "") return;
+    const li = document.createElement("li"); // burada ise yeni bir li elemanı oluşturdum.
+    li.textContent = value; // burada ise li içine yazılacak text content alanına value değeri yazsın dedim.
 
-    const li = document.createElement("li");
-    li.textContent = value;
+    const btn = document.createElement("button"); // burada ise yeni bir buton oluşturdum.
+    btn.textContent = "SİL"; // burada ise butonun text contenti SİL olarak ayarlandı.
 
-    const btn = document.createElement("button");
-    btn.textContent = "SİL";
-
-    btn.onclick = function(){
-        li.remove();
+    btn.onclick = function(){ // burada ise sil butonum için bir fonskiyon oluşturdum.
+        li.remove(); // burada fonksiyonun işlevi yani buton li elemanını kaldırsın dedim.
     }
 
-    li.appendChild(btn);
+    li.appendChild(btn); // burada ise her liste elemanım için birer silme butonu oluşturduk.
 
-    document.getElementById("taskList").appendChild(li);
-    input.value = "";
+    document.getElementById("taskList").appendChild(li); // burada ise taskList yani bizim görevlerimiz yeni li elemanları üzerine eklenecek.
+    input.value = ""; // burada ise her ekleme sonrası input alanı boş olsun istiyoruz.
 }
+
