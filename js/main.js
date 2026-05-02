7cac2d641802f556737d479f68032bd538969a9f
Mert Genç <162042618+Mertgencc@users.noreply.github.com>
2026-05-02 22:52:32 +0300

    LocalStorage Eklendi

--- js/main.js
+++ js/main.js
@@ -1,22 +1,92 @@
-function addTask(){
-    const input = document.getElementById("textInput");  // burada ise ben textInput alanımı çağırdım.
-    const value = input.value; // burada da input alanımın içine oluşturduğum value değerimi girmeye başladım.
-    
-    if (value === "") return; // burada ise kullanıcı değer girmezse ekran bir şey dönmez.
+let tasks = []; // burada global değişken tanımladım. Burası görevlerimizi tutar.
+window.onload = function () {
+  const data = localStorage.getItem("tasks");
 
-    const li = document.createElement("li"); // burada ise yeni bir li elemanı oluşturdum.
-    li.textContent = value; // burada ise li içine yazılacak text content alanına value değeri yazsın dedim.
+  // üst kısım ise tarayıcıdan tasks verilerini çeker. Ama string gelir.
 
-    const btn = document.createElement("button"); // burada ise yeni bir buton oluşturdum.
-    btn.textContent = "SİL"; // burada ise butonun text contenti SİL olarak ayarlandı.
+  if (data) {
+    tasks = JSON.parse(data);
 
-    btn.onclick = function(){ // burada ise sil butonum için bir fonskiyon oluşturdum.
-        li.remove(); // burada fonksiyonun işlevi yani buton li elemanını kaldırsın dedim.
-    }
+    // Burada ise string veriyi array yapıyoruz.
 
-    li.appendChild(btn); // burada ise her liste elemanım için birer silme butonu oluşturduk.
+    renderTasks();
 
-    document.getElementById("taskList").appendChild(li); // burada ise taskList yani bizim görevlerimiz yeni li elemanları üzerine eklenecek.
-    input.value = ""; // burada ise her ekleme sonrası input alanı boş olsun istiyoruz.
+    // sayfa açılınca verileri ekrana yazdırma.
+  }
+};
+
+function addTask() {
+  const input = document.getElementById("textInput"); // burada ise ben textInput alanımı çağırdım.
+  const value = input.value; // burada da input alanımın içine oluşturduğum value değerimi girmeye başladım.
+
+  if (value === "") return; // burada ise kullanıcı değer girmezse ekran bir şey dönmez.
+
+  const li = document.createElement("li"); // burada ise yeni bir li elemanı oluşturdum.
+  li.textContent = value; // burada ise li içine yazılacak text content alanına value değeri yazsın dedim.
+
+  // LOCALSTORAGE İÇİN EKLENECEK
+
+  tasks.push(value);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+
+  const btn = document.createElement("button"); // burada ise yeni bir buton oluşturdum.
+  btn.textContent = "SİL"; // burada ise butonun text contenti SİL olarak ayarlandı.
+
+  btn.onclick = function () {
+    tasks = tasks.filter((t) => t !== value);
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+    renderTasks();
+  };
+
+  li.appendChild(btn); // burada ise her liste elemanım için birer silme butonu oluşturduk.
+
+  renderTasks();
+  input.value = ""; // burada ise her ekleme sonrası input alanı boş olsun istiyoruz.
 }
 
+// RENDER TASK EKLEMEK ZORUNDAYIM
+
+function renderTasks(){
+    const list = document.getElementById("taskList");
+    //html deki liste alanını seçiyoruz.
+
+    list.innerHTML = "";
+    //eski tüm listeyi siliyoruz ki yeni listeyi ekleyebilelim.
+
+    tasks.forEach(function (task){
+
+        // array içindeki her görevi tek tek geziyoruz.
+
+        const li = document.createElement("li");
+
+        // Yeni bir liste elemanı oluşturuyoruz.
+
+        li.textContent = "task";
+
+        // görevin yazısını li içine koyuyoruz.
+
+        const btn = document.createElement("button");
+
+        //silme butonu oluşturduk.
+
+        btn.textContent = "SİL";
+
+        btn.onclick = function(){
+            tasks = tasks.filter((t) => t !== task);
+
+            //tıklanan görevi array den sildik.
+
+            localStorage.setItem("tasks", JSON.stringify(tasks));
+
+            // güncel listeyi tekrar kaydettik.
+            
+            renderTasks();
+
+            //ekranı yeniden çizdik.
+        }
+
+        li.appendChild(btn);
+        list.appendChild(li);
+        
+    })
+}
